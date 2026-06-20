9b102068c9e792cd8d8a96c749747390200b6e9c
Bevara venkatesh <bevaravenkatesh60@gmail.com>
2026-06-20 15:43:51 +0530

    Applicants Database

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Applicant Database models.
+class Applicants(models.Model):
+    User_Id = models.AutoField(primary_key=True)
+    Full_Name = models.CharField(max_length=100)
+    Email = models.EmailField(unique=True)
+    Phone = models.CharField(max_length=10)
+    resume = models.FileField(upload_to='resumes/')
+    Profile_photo = models.ImageField(upload_to='profiles/')
+    Created_at = models.DateTimeField(auto_now_add=True)
+

e9bb3b2702fb6d1592317a4ec37cbfb8cb3ba530
Bevara venkatesh <bevaravenkatesh60@gmail.com>
2026-06-25 16:24:27 +0530

    removed applicants Database

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,2 @@
 from django.contrib import admin
-from .models import Applicants
-# Register your models here.
-admin.site.register(Applicants)
+
--- authentication/models.py
+++ authentication/models.py
@@ -1,12 +1,4 @@
 from django.db import models
 
-# Applicant Database models.
-class Applicants(models.Model):
-    User_Id = models.AutoField(primary_key=True)
-    Full_Name = models.CharField(max_length=100)
-    Email = models.EmailField(unique=True)
-    Phone = models.CharField(max_length=10)
-    resume = models.FileField(upload_to='resumes/')
-    Profile_photo = models.ImageField(upload_to='profiles/')
-    Created_at = models.DateTimeField(auto_now_add=True)
+
 

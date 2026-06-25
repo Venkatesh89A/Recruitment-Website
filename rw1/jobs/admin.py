4b6f69f8c4f11641a5b6a5bb97a3e5a5957c5e8a
Bevara venkatesh <bevaravenkatesh60@gmail.com>
2026-06-25 16:55:16 +0530

    Remove obsolete rw1 project

--- rw1/jobs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Job
-
-
-@admin.register(Job)
-class JobAdmin(admin.ModelAdmin):
-    list_display = ("title", "location", "category", "posted_by", "is_active", "posted_at")
-    list_filter = ("category", "is_active", "location")
-    search_fields = ("title", "description", "location")
